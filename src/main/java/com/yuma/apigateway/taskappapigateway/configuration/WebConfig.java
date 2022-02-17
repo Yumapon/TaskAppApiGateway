3390c39a9dae627fdd5ba4b8ed69ccbc66bf1eb2
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-02-17 22:52:07 +0900

    test

--- src/main/java/com/yuma/apigateway/taskappapigateway/configuration/WebConfig.java
+++ src/main/java/com/yuma/apigateway/taskappapigateway/configuration/WebConfig.java
@@ -47,31 +47,4 @@ public class WebConfig {
     return new AWSXRayServletFilter("TaskAppApiGateway");
   }
 
-  @Bean
-  //WebClientに共通して行うフィルタ処理を定義、実際の処理はexchangeFunction()にて定義
-  public WebClient userWebClient(){
-      return WebClient.builder()
-              .baseUrl("")
-              //.filter(exchangeFilterFunction())
-              .build();
-  }
-
-  //com.amazonaws.xray.AWSXRayから実行中のセグメントオブジェクトを取得します。
-  private ExchangeFilterFunction exchangeFilterFunction(){
-      return (clientRequest, nextFilter) -> {
-          //セグメント情報を取得
-          Segment segment = AWSXRay.getCurrentSegment();
-          Subsegment subsegment = AWSXRay.getCurrentSubsegment();
-          //Traceにヘッダーを追加
-          TraceHeader traceHeader = new TraceHeader(segment.getTraceId(),
-                  segment.isSampled() ? subsegment.getId() : null,
-                  segment.isSampled() ? TraceHeader.SampleDecision.SAMPLED : TraceHeader.SampleDecision.NOT_SAMPLED);
-
-          ClientRequest newClientRequest = ClientRequest.from(clientRequest)
-                  .header(TraceHeader.HEADER_KEY, traceHeader.toString())
-                  .build();
-          return nextFilter.exchange(newClientRequest);
-      };
-  }
-  
 }
