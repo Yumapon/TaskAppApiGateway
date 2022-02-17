6301707579a4c331c309c01ea53bac0ff43d201c
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-02-17 19:09:47 +0900

    test

--- src/main/java/com/yuma/apigateway/taskappapigateway/Service/dto/Task.java
+++ src/main/java/com/yuma/apigateway/taskappapigateway/Service/dto/Task.java
@@ -2,6 +2,8 @@ package com.yuma.apigateway.taskappapigateway.Service.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +19,14 @@ import lombok.Setter;
 public class Task {
 
     private String num;//一意キー
+    
     private String name;//Task名
+
     private String content;//Task内容
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate deadline;//期日
+    
     private String client;//Task依頼主
     
 }
