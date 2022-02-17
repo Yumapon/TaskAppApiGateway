4f4a04d1f22683052f684b1b0d0dc02008e34b42
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-02-17 19:19:50 +0900

    test

--- src/main/java/com/yuma/apigateway/taskappapigateway/Service/dto/Task.java
+++ src/main/java/com/yuma/apigateway/taskappapigateway/Service/dto/Task.java
@@ -3,6 +3,8 @@ package com.yuma.apigateway.taskappapigateway.Service.dto;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,11 +21,12 @@ import lombok.Setter;
 public class Task {
 
     private String num;//一意キー
-    
+
     private String name;//Task名
 
     private String content;//Task内容
 
+    @JsonSerialize(using = LocalDateSerializer.class)
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate deadline;//期日
     
