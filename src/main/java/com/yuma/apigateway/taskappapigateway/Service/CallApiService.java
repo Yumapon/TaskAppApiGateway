55d0312a97cf28312b8b7cd8685c602cf45af4b1
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-02-17 22:56:15 +0900

    test

--- src/main/java/com/yuma/apigateway/taskappapigateway/Service/CallApiService.java
+++ src/main/java/com/yuma/apigateway/taskappapigateway/Service/CallApiService.java
@@ -31,7 +31,6 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.AllArgsConstructor;
 
@@ -41,11 +40,7 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class CallApiService {
-
-    //API呼び出し用クラス
-    @Autowired
-    private WebClient webClient;
-
+    
     //API情報の定義クラス
     @Autowired
     private APIDestinations apiDestinations;
