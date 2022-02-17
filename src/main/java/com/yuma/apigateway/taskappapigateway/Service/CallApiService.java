f973ff72f36c0817bdb8afe2e1f34628939aac83
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-02-17 22:29:26 +0900

    test

--- src/main/java/com/yuma/apigateway/taskappapigateway/Service/CallApiService.java
+++ src/main/java/com/yuma/apigateway/taskappapigateway/Service/CallApiService.java
@@ -7,17 +7,13 @@ import java.io.OutputStream;
 */
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
-import java.util.Base64;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.entities.Segment;
 import com.amazonaws.xray.proxies.apache.http.HttpClientBuilder;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.yuma.apigateway.taskappapigateway.Service.dto.GetTaskAllResDto;
 import com.yuma.apigateway.taskappapigateway.Service.dto.Task;
 import com.yuma.apigateway.taskappapigateway.error.NoNormalResponseError;
@@ -34,18 +30,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import lombok.AllArgsConstructor;
-import reactor.core.publisher.Flux;
 
 /**
  * 外部API呼び出しを定義するクラス
@@ -76,18 +64,57 @@ public class CallApiService {
 
         logger.info("start call getTask API");
 
-        //Headerの設定
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        //返すタスククラス
+        Task task = null;
 
-        //URiの作成
-        String uri = apiDestinations.getTaskapiurlGet() + "/" + id; 
- 
-        Task task = webClient.get().uri(uriBuilder -> uriBuilder.path(uri).queryParam("email", email).build())
-                        .retrieve()
-                        .bodyToMono(Task.class)
-                        .block();
+        try(CloseableHttpClient httpClient = HttpClientBuilder.create().build(); ){
 
+            //URIの作成
+            URI uri = new URIBuilder(apiDestinations.getTaskapiurlGet() + "/" + id)
+                        .addParameter("email", email)
+                        .build(); 
+
+            //Segmentの情報を取得
+            Segment segment = AWSXRay.getCurrentSegment();
+            String traceId = segment.getTraceId().toString();
+            logger.debug("TRACEID: " + traceId);
+            
+            //HTTPリクエストの設定
+            //一応タイムアウト設定を実装
+            RequestConfig config = RequestConfig.custom()
+                                                .setSocketTimeout(3000)
+                                                .setConnectTimeout(3000)
+                                                .build();
+
+            // HTTPのGETリクエストを構築
+            HttpGet httpGet = new HttpGet(uri);
+            httpGet.setHeader("trace-id", traceId);
+            httpGet.setConfig(config);
+
+            //HTTPリクエストを実行する。HTTPステータスが200の場合のみOKとする
+            try( CloseableHttpResponse response = httpClient.execute(httpGet); ){
+
+                logger.debug("StatusCode: " + response.getStatusLine().getStatusCode());
+
+                if( response.getStatusLine().getStatusCode() == HttpStatus.SC_OK ) {
+                    HttpEntity entity = response.getEntity();
+                    String resStr = EntityUtils.toString(entity);
+                    logger.debug("JsonString: " + resStr);
+                    
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    task = mapper.readValue(resStr, Task.class);
+                }
+                else {
+                    logger.error("Status Codeが200以外で帰ってきました");
+                    task = new Task();
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
         logger.info("end call getTaskAPI");
         
@@ -116,6 +143,11 @@ public class CallApiService {
             URI uri = new URIBuilder(apiDestinations.getTaskapiurlGetall())
                         .addParameter("email", email)
                         .build(); 
+
+            //Segmentの情報を取得
+            Segment segment = AWSXRay.getCurrentSegment();
+            String traceId = segment.getTraceId().toString();
+            logger.debug("TRACEID: " + traceId);
             
             //HTTPリクエストの設定
             //一応タイムアウト設定を実装
@@ -126,31 +158,26 @@ public class CallApiService {
 
             // HTTPのGETリクエストを構築
             HttpGet httpGet = new HttpGet(uri);
+            httpGet.setHeader("trace-id", traceId);
             httpGet.setConfig(config);
 
             //HTTPリクエストを実行する。HTTPステータスが200の場合のみOKとする
             try( CloseableHttpResponse response = httpClient.execute(httpGet); ){
 
+                logger.debug("StatusCode: " + response.getStatusLine().getStatusCode());
+
                 if( response.getStatusLine().getStatusCode() == HttpStatus.SC_OK ) {
                     HttpEntity entity = response.getEntity();
                     String resStr = EntityUtils.toString(entity);
+                    logger.debug("JsonString: " + resStr);
                     
                     ObjectMapper mapper = new ObjectMapper();
 
                     tasks = mapper.readValue(resStr, GetTaskAllResDto.class);
-
-                    //Debug用
-                    System.out.println("##########Debug#########");
-                    System.out.println(entity);
-                    //Debug用
-                    System.out.println("##########Debug2#########");
-                    System.out.println(resStr);
-                    //Debug用
-                    System.out.println("##########Debug3#########");
-                    System.out.println(tasks);
                 }
                 else {
-
+                    logger.error("Status Codeが200以外で帰ってきました");
+                    tasks = new GetTaskAllResDto();
                 }
             } catch(Exception e) {
                 e.printStackTrace();
@@ -179,24 +206,48 @@ public class CallApiService {
 
         logger.info("start call dnmonster api");
 
-        //Headerの設定
-        //HttpHeaders headers = new HttpHeaders();
+        byte[] identidock = null;
 
-        //URLの作成
-        String uri = apiDestinations.getDnmonsterGet() + "/" + email; 
+        try(CloseableHttpClient httpClient = HttpClientBuilder.create().build(); ){
 
-        byte[] image = DataBufferUtils.join(webClient.get().uri(uriBuilder -> uriBuilder.path(uri).queryParam("size", size).build())
-                        .retrieve()
-                        .bodyToFlux(DataBuffer.class))
-                        .map(dataBuffer -> {
-                            byte[] bytes = new byte[dataBuffer.readableByteCount()];
-                            dataBuffer.read(bytes);
-                            DataBufferUtils.release(dataBuffer);
-                            return bytes;
-                        })
-                        .block();
+            //URIの作成
+            URI uri = new URIBuilder(apiDestinations.getDnmonsterGet() + "/" + email)
+            .addParameter("size", String.valueOf(size) )
+            .build();
 
-        return image;
+            //HTTPリクエストの設定
+            //一応タイムアウト設定を実装
+            RequestConfig config = RequestConfig.custom()
+                                                .setSocketTimeout(3000)
+                                                .setConnectTimeout(3000)
+                                                .build();
+            
+            // HTTPのGETリクエストを構築
+            HttpGet httpGet = new HttpGet(uri);
+            httpGet.setConfig(config);
+
+            //HTTPリクエストを実行する。HTTPステータスが200の場合のみOKとする
+            try( CloseableHttpResponse response = httpClient.execute(httpGet); ){
+
+                if( response.getStatusLine().getStatusCode() == HttpStatus.SC_OK ) {
+                    HttpEntity entity = response.getEntity();
+                    String resStr = EntityUtils.toString(entity);
+                    
+                    ObjectMapper mapper = new ObjectMapper();
+
+                    identidock = mapper.readValue(resStr, byte[].class);
+                }
+                else {
+                    logger.error("Status Codeが200以外で帰ってきました");
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return identidock;
     }
     
 }
